12bdb9a30385db1ef44ce9296ec2f89928cca94b
pidgezero_one <s.kischak@gmail.com>
2026-04-26 23:15:43 -0400

    source

--- src/35/contents/script_0x350000.py
+++ src/35/contents/script_0x350000.py
@@ -12,7 +12,7 @@ from ....enemies.enemies import *
 from ....enemy_attacks.attacks import *
 from smrpgpatchbuilder.datatypes.battle_animation_scripts.arguments.battle_targets import *
 
-script = AnimationScriptBlock(expected_size=14, expected_beginning=0x350000, script=[
+script = AnimationScriptBlock(expected_size=0x202, expected_beginning=0x350000, script=[
 	DefineObjectQueue(["ally_behaviour_pointers"], identifier="command_0x350000"),
 	DefineObjectQueue(["command_0x350402", "command_0x350412", "command_0x350422", "command_0x350432", "command_0x350442", "command_0x350402"], identifier="ally_behaviour_pointers")
 ])
